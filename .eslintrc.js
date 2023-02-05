75d93da1cab06351025c7cfd6366a60c9fd49873
Gavin Mogan <git@gavinmogan.com>
2023-02-04 17:59:52 -0800

    upgrade to external theme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,65 @@
 module.exports = {
-  "root": true,
-  "parser": "babel-eslint",
-  "extends": [
-    "standard",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:jest/recommended"
+  root: true,
+  extends: [
+    'react-app',
+    'standard',
+    'eslint:recommended',
+    'plugin:mdx/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
-  "plugins": [
-    "babel",
-    "import",
-    "jest",
-    "jsx-a11y",
-    "promise",
-    "react",
-    "standard"
+  plugins: [
+    'babel',
+    'import',
+    'jsx-a11y',
+    'promise',
+    'react',
+    'standard'
   ],
-  "settings": {
-    'import/extensions': [ '.js', '.jsx' ],
-    'import/resolver': {'node': {'extensions': ['.js', '.jsx']}},
-    "react": { "version": "detect" },
-    "linkComponents": [
-      {"name": "Link", "linkAttribute": "to"}
-    ]
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+    'import/resolver': { node: { extensions: ['.js', '.jsx'] } },
+    react: { version: 'detect' },
+    linkComponents: [
+      { name: 'Link', linkAttribute: 'to' }
+    ],
+    'mdx/code-blocks': true
   },
-  "rules": {
-    "semi": [2, "always"],
-    "react/jsx-no-undef": ["error"],
-    "no-undef": ["error"],
-    "no-var": "error",
-    "no-func-assign": "error",
-    "no-class-assign": "error",
-    "import/no-cycle": "error",
-    "import/no-self-import": "error",
-    "import/no-absolute-path": "error",
-    "import/no-webpack-loader-syntax": "error",
-    "import/no-mutable-exports": "error",
-    "import/no-duplicates": "error",
+  rules: {
+    semi: [2, 'always'],
+    'react/jsx-no-undef': ['error'],
+    'no-undef': ['error'],
+    'no-var': 'error',
+    'no-func-assign': 'error',
+    'no-class-assign': 'error',
+    'import/no-cycle': 'error',
+    'import/no-self-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-duplicates': 'error',
     // "jsx-a11y/anchor-is-valid": [ "error", { "components": [ "Link" ], "specialLink": [ "to" ] } ],
-    "babel/new-cap": 1,
-    "babel/no-invalid-this": 1,
-    "babel/semi": [2, "always"],
-    "babel/no-unused-expressions": 1,
-    "babel/valid-typeof": 1,
-    "react/prefer-stateless-function": "off",
-    "react/no-children-prop": "error",
-    "react/prop-types": "off",
-    "react/no-danger": "off"
+    'react/prefer-stateless-function': 'off',
+    'react/no-children-prop': 'off',
+    'react/prop-types': 'off',
+    'react/no-danger': 'off'
   },
-  "env": {
-    "browser": true,
-    "es6": true
+  globals: {
+    __PATH_PREFIX__: true,
+    Text: true,
+    Title: true
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module'
   }
-}
+};
